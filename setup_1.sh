35e0c716fb1593a01f18f7166c2bf772f327bde4
Teng Jiang <jiangteng0403@gmail.com>
2023-03-01 16:24:00 -0500

    Delete setup_1.sh

--- setup_1.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-# disable nouveau
-# could see if lsmod | grep nouveau produces anything first, but in the context of cloudlab, we do not need to do this.
-sudo touch /etc/modprobe.d/blacklist-nouveau.conf
-sudo echo "blacklist nouveau" > /etc/modprobe.d/blacklist-nouveau.conf
-sudo echo "options nouveau modeset=0" >> /etc/modprobe.d/blacklist-nouveau.conf
-sudo update-initramfs -u
-sudo reboot
